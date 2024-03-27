2af570ea02ddb97181847678720d97fba8de479b
Thomas Frade <thomas.frade@usp.br>
2024-03-27 17:02:37 -0300

    Data Sync added

--- data_handler.py
+++ data_handler.py
@@ -10,8 +10,6 @@ def fetch_daily_tasks():
     return datalist[1:]
 
 
-
-
 def fetch_lastweek():
     lastweekdates = []
     for c in range(6, 0, -1):
@@ -23,6 +21,25 @@ def fetch_lastweek():
         lastweekdays.append(weekday)
 
     return lastweekdates, lastweekdays
+
+
+def recreate_dailytasks(task_list):
+    with open('dailytask.csv', 'w') as newtaskscsv:
+        headers = 'name,description,recurrence,date_list\n'
+        newtaskscsv.write(headers)
+
+        for task in task_list:
+            current_date = date.isoformat(date.today())
+            if task[4] == 1 and current_date not in task[3]:
+                task[3] += ';' + current_date
+            elif task[4] == 0 and current_date in task[3]:
+                task[3] = task[3][:-11]
+            task.pop(4)
+
+            task_line = f'{task[0]}'
+            for x in task[1:]:
+                task_line += ',' + x
+            newtaskscsv.write(task_line+'\n')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class GUI:
         self.current_frame = None
         self.fetch_frame = None
 
+        # Root Variables
+        self.bar = None
+
         # Fetching data
         self.daily_tasks = fetch_daily_tasks()
         for task in self.daily_tasks:
-            task.append(tk.IntVar)  # Creating a completion tracker for every task
+            task.append(tk.IntVar())  # Creating a completion tracker for every task
+            if task[2][date.today().weekday()] == 'T' and date.isoformat(date.today()) in task[3]:
+                task[4].set(1)
 
         # Initializing Tab Method
         self.dailytask_frame()
@@ -35,31 +40,34 @@ class GUI:
         self.fetch_frame = tk.Frame(self.current_frame)
 
         # Dashboard Widget Creation
-        dashboard_base = tk.LabelFrame(self.current_frame, text='Daily Tasks Dashboard', height=300)
-        dashboard_base.pack_propagate(False)
+        dashboard_base = tk.LabelFrame(self.current_frame, text='Daily Tasks Dashboard', height=300, width=500)
+        dashboard_base.grid_propagate(False)
 
-        # Daily Progress Bar
-        bar = ttk.Progressbar(dashboard_base, orient='horizontal', mode='determinate')
-        bar.pack(fill='x', padx=50)
-        bar['value'] = 87
+        # Title Cards Creation
+        bar_label = tk.Label(dashboard_base, text='Progress', width=10)
+        bar_label.grid(row=0, column=0, padx=10)
+
+        tasks_label = tk.Label(dashboard_base, text='Due Tasks', width=45)
+        tasks_label.grid(row=0, column=1, padx=10)
 
         # Due Tasks Board
         due_tasks_board = tk.Listbox(dashboard_base)
+        due_tasks_board.pack_propagate(False)
         for task in self.daily_tasks:
             if task[2][date.today().weekday()] == 'T':
                 task_option = tk.Button(due_tasks_board, text=task[0],
                                         command=self.widget_fetch(task))
                 task_option.pack(fill='x')
 
-        due_tasks_board.pack(fill='x')
+        # Daily Progress Bar
+        self.bar = ttk.Progressbar(dashboard_base, orient='vertical', mode='determinate', length=225)
+        self.bar.grid(row=1, column=0, pady=15, padx=25, sticky='we')
+        self.update_bar()
+
+        due_tasks_board.grid(row=1, column=1, sticky='news')
         dashboard_base.pack(fill='x')
 
         self.current_frame.pack()
-
-    def widget_fetch(self, element, widget=''):
-        # Temporary fix for Daily Task Dashboards
-
-        return lambda: self.daily_task_widget(element)
 
     def daily_task_widget(self, info):
         # Widget Base creation
@@ -93,16 +101,49 @@ class GUI:
                 weekbutton.image = red_x
             else:
                 # If task wasn't supposed to be completed, add gray circle
-                weekbutton = tk.Button(self.fetch_frame, text=pretty_day, image=gray_o, compound=tk.TOP, width=50)
+                weekbutton = tk.Button(self.fetch_frame, text=pretty_day, image=gray_o, compound=tk.TOP, width=50,
+                                       command=self.save_sequence)
                 weekbutton.image = gray_o
 
             weekbutton.grid(row=c//3, column=(c % 3)+1)
 
         # isCompleted Checkbox add_on
-        iscompleted_check = tk.Checkbutton(self.fetch_frame, text='Was it done today?', variable=info[4])
-        iscompleted_check.grid(row=3, column=1, columnspan=6, pady=10)
+        iscompleted_check = tk.Checkbutton(self.fetch_frame, text='Was it done today?', variable=info[4],
+                                           command=self.update_bar)
+        iscompleted_check.grid(row=3, column=1, columnspan=5, pady=10)
 
         self.fetch_frame.pack(fill='x')
+
+    def widget_fetch(self, element, widget=''):
+        # Temporary fix for Daily Task Dashboards
+
+        return lambda: self.daily_task_widget(element)
+
+    def fetch_done_tasks(self):
+        done_tasks = []
+        for task in self.daily_tasks:
+            if task[4].get():
+                done_tasks.append(task)
+
+        return done_tasks
+
+    def update_bar(self):
+        done_tasks = len(self.fetch_done_tasks())
+        due_tasks = 0
+        for task in self.daily_tasks:
+            if task[2][date.today().weekday()] == 'T':
+                due_tasks += 1
+
+        self.bar['value'] = (done_tasks/due_tasks * 100)//1
+
+    def save_sequence(self):
+        # Converting TK variables into numbers
+        f_tasks = []
+        for task in self.daily_tasks:
+            f_task = task[:4]
+            f_task.append(task[4].get())
+            f_tasks.append(f_task)
+        recreate_dailytasks(f_tasks)
 
 
 def main():
